1ce39d383ecd6f593d084a803a280ba4449ac212
Prakash Mishra <mishraprakashcpn47@gmail.com>
2025-06-12 02:07:14 +0530

    changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
+// postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {}, // Use the official Tailwind CSS PostCSS plugin for v4.1
+    autoprefixer: {}, // Optional: for vendor prefixes
   },
-}
-
+};
